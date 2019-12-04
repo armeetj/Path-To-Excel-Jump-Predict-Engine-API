2fd9b2168060e34db5d295dd4e2287f2450c901c
Armeet Jatyani <xarmeetx@gmail.com>
2019-12-03 22:34:08 -0800

    changes

--- api/api.py
+++ api/api.py
@@ -1,12 +1,19 @@
-#imports
+#==============================
+#           IMPORTS
+#==============================
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
 
+#==============================
+#            CONFIG
+#==============================
 #make new flask app
 app = Flask(__name__)
 
 SECRET_KEY = ""
-SQLALCHEMY_DATABASE_URI = "sqlite:///db/db.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:///../db/db.db"
 
 #app key and db location
 app.config['SECRET_KEY'] = SECRET_KEY
@@ -15,6 +22,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 #create new db
 db = SQLAlchemy(app)
 
+#==============================
+#         DB CLASSES
+#==============================
 #user
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -30,10 +40,41 @@ class Todo(db.Model):
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer)
 
-#app routes
+#==============================
+#          APP ROUTES
+#==============================
+#GET    /
 @app.route('/', methods=['GET'])
+def redirect_to_api():
+    return "make requests to /api instead"
+@app.route('/api/', methods=['GET'])
 def home():
     return "Home Page!"
+
+#GET    /user/
+@app.route('/api/user', methods=['GET'])
+def get_all_users():
+    return 'placeholder'
+
+#GET    /user/username 
+@app.route('/api/user/<user_id>', methods=['GET'])
+def get_one_user():
+    return 'placeholder'
+
+#POST   /user/
+@app.route('/api/user', methods=['POST'])
+def create_user():
+    return 'placeholder'
+
+#PUT    /user/username
+@app.route('/api/user/<user_id>', methods=['PUT'])
+def promote_user():
+    return 'placeholder'
+
+#DELETE /user/username
+@app.route('/api/user/<user_id>', methods=['DELETE'])
+def delete_user():
+    return 'placeholder'
 
 #run app 
 if __name__ == '__main__'  :
